e0f30ad5888e5fb25656225ba4f438954df196cc
Ari Mauri <ari.mauri.jr@gmail.com>
2023-03-06 14:12:45 -0300

    Coverte zero

--- BinarioComRecursao/Program.cs
+++ BinarioComRecursao/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -8,32 +10,44 @@
         int[] vetor = new int[8];
         int indice = 0;
 
-        Console.WriteLine("Digite um valor: ");
+        Console.WriteLine("DIGITE UM NÚMERO: ");
         numero = int.Parse(Console.ReadLine());
+
+        while(numero < 0)
+        {
+            Console.WriteLine("DIGITE UM NÚMERO: ");
+            numero = int.Parse(Console.ReadLine());
+        }
 
         Conversao(numero);
 
         int[] Conversao(int quociente)
         {
-           
-            if(quociente ==  1)
+            if (quociente == 0)
             {
-                vetor[indice] = quociente;
                 return vetor;
             }
             else
             {
-                vetor[indice] = quociente % 2;
-                indice++;
-                quociente = quociente / 2;
-                return Conversao(quociente);
+                if (quociente == 1)
+                {
+                    vetor[indice] = quociente;
+                    return vetor;
+                }
+                else
+                {
+                    vetor[indice] = quociente % 2;
+                    indice++;
+                    //quociente = quociente / 2;
+                    return Conversao(quociente / 2);
+                }
             }
         }
 
+        
         for(int i = 7; i >= 0; i--) 
         {
             Console.Write(vetor[i]);
         }
-
     }
 }
